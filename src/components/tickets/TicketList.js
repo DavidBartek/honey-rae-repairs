042f946cccbfbd0b6be3e324e07ae721d5f0f19a
David Bartek <davidwbartek@gmail.com>
2023-07-10 17:04:28 -0500

    completed through chapter 8

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -3,26 +3,25 @@ import "./Tickets.css"
 import { useNavigate } from "react-router-dom"
 
 export const TicketList = () => {
+    
+    // *** useState() hooks (also useNavigate()) ***
+    
     const [tickets, setTickets] = useState([])
     const [filteredTickets, setFiltered] = useState([])
     const [emergency, setEmergency] = useState(false)
+    const [openOnly, updateOpenOnly] = useState(false)
     const navigate = useNavigate()
+
+    // accesses the "honeyUserObject" which has been passed in to local storage as "honey_user."
+    // cf. Register.js, Login.js for the creation of this. 
 
     const localHoneyUser = localStorage.getItem("honey_user")
     const honeyUserObject = JSON.parse(localHoneyUser)
 
-    useEffect(
-        () => {
-            if (emergency) {
-                const emergencyTickets = tickets.filter(ticket => ticket.emergency === true)
-                setFiltered(emergencyTickets)
-            } else {
-                setFiltered(tickets)
-            }
-        },
-        [emergency]
-    )
+    // *** useEffect() hooks ***
 
+    // fetches serviceTickets data (array) from API; invoking setTickets to set tickets state to hold this data.
+    // dependencies array is empty; therefore componenent's initial state is being observed.
     useEffect(
         () => {
             fetch (`http://localhost:8088/serviceTickets`)
@@ -31,9 +30,11 @@ export const TicketList = () => {
                     setTickets(ticketArray)
                 })
         },
-        [] // when this dependencies array is empty, you are observing initial component state
+        []
     )
     
+    // assigns different state to tickets depending on if user if staff or not. Staff: all tix. Non-staff: filtered.
+    // dependencies array is watching for when tickets state is modified.
     useEffect(
         () => {
             console.log(tickets)
@@ -49,15 +50,58 @@ export const TicketList = () => {
         },
         [tickets]
     )
+
+    // if emergency state is set to true, filter tickets array by emergency property = true.
+    // dependencies array is watching for when emergency state is modified.
+    useEffect(
+        () => {
+            if (emergency) {
+                const emergencyTickets = tickets.filter(ticket => ticket.emergency === true)
+                setFiltered(emergencyTickets)
+            } else {
+                setFiltered(tickets)
+            }
+        },
+        [emergency]
+    )
     
+    // if openOnly is set to true, filter tickets array by tickets from that user AND dateCompleted is empty string
+    // (i.e., show that user's open tickets)
+    // if openOnly is set to false, filter tickets array by displaying all tickets of that user
+    // dependencies array is watching for when openOnly state is modified.
+    useEffect(
+        () => {
+            if (openOnly) {
+                const openTicketArray = tickets.filter(ticket => {
+                    return ticket.userId === honeyUserObject.id && ticket.dateCompleted === ""
+                })
+                setFiltered(openTicketArray)
+            } else {
+                const myTickets = tickets.filter(ticket => ticket.userId === honeyUserObject.id)
+                setFiltered(myTickets)
+            }
+            
+        },
+        [openOnly]
+    )    
+
+
+    // *** HTML OF TICKET LIST PAGE ***
+    // staff will display buttons to toggle "Emergency Only" and "Show All"
+    // non-staff will display buttons to navigate to "Create Ticket," toggle his "Open Tickets" and "All My Tickets"
+
     return <>
         {
             honeyUserObject.staff
                 ? <>
-                <button onClick={ () => {setEmergency(true) } } >Emergency Only</button>
-                <button onClick={ () => {setEmergency(false) } } >Show All</button>
+                    <button onClick={ () => {setEmergency(true)}} >Emergency Only</button>
+                    <button onClick={ () => {setEmergency(false)}} >Show All</button>
                 </>
-                : <button onClick={() => navigate("/ticket/create")}>Create Ticket</button>
+                : <>
+                    <button onClick={() => navigate("/ticket/create")}>Create Ticket</button>
+                    <button onClick={() => updateOpenOnly(true)}>Open Tickets</button>
+                    <button onClick={() => updateOpenOnly(false)}>All My Tickets</button>
+                </>
         }
 
         
